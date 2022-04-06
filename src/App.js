fa43907b04034bed52c7afdeccc814495de7cce9
Afsana <afsana.abida@stud.uni-bamberg.de>
2022-04-06 12:27:40 +0200

    ChatFeed created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { ChatEngine } from "react-chat-engine";
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
 
 import './App.css';
 
@@ -10,6 +12,7 @@ const App = () => {
             projectID="0d391b3d-f16b-477e-9120-441b3b8e34ca"
             userName="loficoder"
             userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
        />
     );
 }
